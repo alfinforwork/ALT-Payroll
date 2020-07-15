a208b9f72847a7d175ae992574ce21c16f7ab29b
Mohamad Alfin Nahrowi <alfinforwork@gmail.com>
2020-07-15 19:05:51 +0700

    Update penggajian.sql

--- penggajian.sql
+++ penggajian.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2020 at 11:57 AM
+-- Generation Time: Jul 15, 2020 at 02:05 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
